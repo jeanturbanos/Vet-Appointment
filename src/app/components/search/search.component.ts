7a9b16e404278b1bb9ff5b57c2befeeb4eecc577
Jeannelou Turbanos <jeanturbanos@gmail.com>
2019-11-05 21:32:34 -0500

    Initial Commit

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,35 @@
+ import
+  { Component,
+     OnInit,
+     Input,
+     Output,
+     EventEmitter    
+    } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit {
+  query:string;
+  @Input() orderBy;
+  @Input() orderType;
+  @Output() queryEvt = new EventEmitter<string>();
+  @Output() orderEvt = new EventEmitter<string>();
+
+  handleQuery(query: string){
+    this.queryEvt.emit(query);
+  }
+
+  handleSort(orderItems){
+    this.orderBy = orderItems.orderBy;
+    this.orderType = orderItems.orderType;
+    this.orderEvt.emit(orderItems);
+  }
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
